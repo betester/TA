830206b93504301bf03504ed9ec0ba8622cad088
Veloraine <bivanderr@gmail.com>
2024-03-30 20:56:39 +0700

    add moke-master configuration

--- moke/__main__.py
+++ moke/__main__.py
@@ -1,16 +1,32 @@
 from flask import Flask, request, abort
-from linebot.v3 import LineBotApi, WebhookHandler
-from linebot.v3.exceptions import InvalidSignatureError
-from linebot.v3.models import MessageEvent, TextMessage, TextSendMessage
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import os
 
+from master.component import MasterComponent
+from confluent_kafka import Producer
+from model.crawler_contract import CrawlerResponse
+
 app = Flask(__name__)
+
+component = MasterComponent()
+producer_observer = component.producer_observer()
+
+_producer_conf = {
+    'bootstrap.servers': os.environ.get("BOOTSTRAP_SERVERS", ""),
+    'client.id': os.environ.get("CLIENT_ID", ""),
+}
+
+producer = Producer(**_producer_conf)
+
 
 CHANNEL_SECRET = os.environ.get("CHANNEL_SECRET", "")
 CHANNEL_ACCESS_TOKEN = os.environ.get("CHANNEL_ACCESS_TOKEN", "")
 
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
+
 
 @app.route("/", methods=["GET", "POST"])
 def hello():
@@ -29,6 +45,16 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text = event.message.text
+
+    producer.send(topic="analyzer", data=CrawlerResponse(message = text, source="MOKE").model_dump_json().encode())
+    producer_observer.send_total_successful_messages(
+        "analyzer",
+        1,
+        lambda x, y: producer.produce(
+            topic=x,
+            value=y
+        )
+    )
     reply = f"You said: {text}"
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
 
