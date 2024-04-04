7440afb9f05a20aa06acde3d4c88f015bfacfc10
Veloraine <bivanderr@gmail.com>
2024-04-04 22:21:21 +0700

    add mocking send command

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -1,5 +1,8 @@
 import discord
 from fogverse.fogverse_logging import get_logger
+import asyncio
+import os
+import csv
 
 class DiscordClient(discord.Client):
     def __init__(self, intents, messages, producer):
@@ -7,6 +10,7 @@ class DiscordClient(discord.Client):
         self.messages = messages
         self.__log = get_logger(name=self.__class__.__name__)
         self.producer = producer
+        self.delay = None
 
     async def on_ready(self):
         self.__log.info(f'{self.user} has connected to Discord!')
@@ -14,14 +18,41 @@ class DiscordClient(discord.Client):
     async def on_message(self, message):
         if message.author == self.user:
             return
+        
+        if message.content.startswith('$start'):
+            # usage $start <delay>
+            try:
+                delay = message.content.split(" ")[1]
+                self.delay = int(delay)
+                self.loop.create_task(
+                    self.mock_send()
+                )
+            except:
+                self.delay = None
 
-        self.__log.info(f"Message from {message.author}: {message.content}")
-        await self.messages.put(message.content)
+        elif message.content == '$stop':
+            # usage $stop
+            self.delay = None
+
+        else:
+            self.__log.info(f"Message from {message.author}: {message.content}")
+            await self.messages.put(message.content)
 
     async def setup_hook(self):
         self.loop.create_task(
             self.producer.run()
         )
+        
+    async def mock_send(self):
+        csv_paths = [os.path.join('./data/crawler/kaggle', file) for file in os.listdir('./data/crawler/kaggle') if file.endswith('.csv')]
+        for csv_path in csv_paths:
+            with open(csv_path, 'r') as csv_file:
+                res = csv.reader(csv_file)
+                for row in res:
+                    await asyncio.sleep(self.delay)
+                    await self.messages.put(row[3])
+        
+
 
 
 
--- discord_bot/producer.py
+++ discord_bot/producer.py
@@ -2,15 +2,18 @@ import asyncio
 from fogverse import Producer, Profiling
 from model.crawler_contract import CrawlerResponse
 from fogverse.util import get_config
+import socket
 
 
 class DiscordProducer(Producer, Profiling):
-    def __init__(self, messages):
+    def __init__(self, messages, producer_observer):
         self.producer_topic = str(get_config("DISCORD_PRODUCER_TOPIC", self, "analyze_v1"))
         self.producer_servers = str(get_config("DISCORD_PRODUCER_SERVERS", self, "localhost:9092"))
         self.group_id = str(get_config("DISCORD_CONSUMER_GROUP_ID", self, "crawler"))
         self.messages = messages
         self._closed = False
+        self._observer = producer_observer
+        self.client_id = socket.gethostname()
         Producer.__init__(self)
         Profiling.__init__(self, name='discord-logs', dirname='discord-logs')
 
@@ -30,3 +33,13 @@ class DiscordProducer(Producer, Profiling):
     
     def encode(self, data: CrawlerResponse) -> bytes:
         return data.model_dump_json().encode()
+    
+    async def send(self, data: CrawlerResponse):
+        result = await super().send(data=data)
+        await self._observer.send_total_successful_messages(
+            target_topic=self.producer_topic,
+            send = lambda x, y: self.producer.send(topic=x, value=y),
+            total_messages = 1
+        )
+        return result
+
