a081aeebe417ef1f5b8a8be1dd951afd1c689c65
betester <ikramzulkarnaen02@gmail.com>
2024-04-20 17:13:11 +0700

    Include keyword analysis process

--- analyzer/processor.py
+++ analyzer/processor.py
@@ -15,11 +15,13 @@ class AnalyzerProcessor(Processor):
         try:
             crawler_results = list(map(lambda message : CrawlerResponse.model_validate_json(message.value()).message, messages))
             disaster_messages = self.analyzer.analyze("is_disaster", crawler_results)
+            keyword_messages = self.analyzer.analyze("keyword", crawler_results)
 
             return [
                 DisasterAnalyzerResponse(
                     text=crawler_results[i],
-                    is_disaster=disaster_messages[i]
+                    is_disaster=disaster_messages[i],
+                    keyword=keyword_messages[i]
                 ).model_dump_json().encode() 
                 for i in range(len(crawler_results)
             )]
